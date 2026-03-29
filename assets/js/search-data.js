bb254736330efd0e4f31df53982e537de0b3a4c8
zjjennings <zjjennings@smu.edu>
2026-03-29 22:33:00 +0000

    Deploying to gh-pages from @ zjjennings/zjjennings.github.io@306e2d42f3f31a1a165e4a19a51f7410ddc98f11 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,9 +456,9 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-undergrad-tableau-project",
+          title: 'Undergrad Tableau Project',
+          description: "Analyzing Texas Government Salaries",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
